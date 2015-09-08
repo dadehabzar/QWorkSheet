27d991acc300dd72df51b720c543cd735170b724
mohsenkhavari <mohsen1391@gmail.com>
2015-09-08 16:24:48 +0430

    Update mainwindow.h

--- example/mainwindow.h
+++ example/mainwindow.h
@@ -22,21 +22,17 @@ private:
     Ui::MainWindow *ui;
 
 signals:
-    void sigAddSheets(QStringList sheet_names);
-    void sigAddColumns(QString sheet_name, QStringList columns_name );
+    void sigAddSheets(QStringList sheet_names, QStringList titles, QStringList x_axis_labels, QStringList y_axis_labels,
+                       QList<int> real_display_precision, QList<bool> have_x_axis, QList<bool> have_y_axis,
+                       QList<bool> reverse_x_axis, QList<bool> reverse_y_axis);
+    void sigAddColumns(QString sheet_name, QStringList columns_name ,QList<QColor> columns_color);
     void sigAddRows(QString sheet_name, QString column_name, QStringList data_pack);
-public slots:
-    void add();
-private slots:
-    void on_pushButton_clicked();
 
-    void on_pushButton_2_clicked();
-    void on_pushButton_3_clicked();
-    void on_pushButton_4_clicked();
-    //void on_checkBox_clicked();
-    void on_checkBox_stateChanged(int arg1);
-   // void on_MainWindow_iconSizeChanged(const QSize &iconSize);
-    void on_pushButton_5_clicked();
+private slots:
+
+    void on_pushButton_addSheets_clicked();
+    void on_pushButton_addCoulms_clicked();
+    void on_pushButton_addRows_clicked();
 };
 
 #endif // MAINWINDOW_H
